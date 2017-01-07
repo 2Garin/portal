e5b8d9fa843b5e3570641b034cc6fbe6d24fc22c
Alexander Ostroverkhov <alexruw@gmail.com>
2017-01-07 13:21:49 +0300

    check db type in migrations

--- migrations/m161218_102640_create_status_table.php
+++ migrations/m161218_102640_create_status_table.php
@@ -14,13 +14,19 @@ class m161218_102640_create_status_table extends Migration
      */
     public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable(self::STATUS, [
             'status_id' => $this->primaryKey()->unsigned(),
             'user_id' => $this->string()->notNull(),
             'date' => $this->dateTime()->defaultValue(null),
             'status' => $this->boolean()->defaultValue(0),
             'response' => $this->text(),
-        ], 'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        ], $tableOptions);
 
         $this->createIndex('uk_status_user_id_date', self::STATUS, ['user_id', 'date'], true);
     }
--- migrations/m161218_102708_create_period_table.php
+++ migrations/m161218_102708_create_period_table.php
@@ -14,12 +14,18 @@ class m161218_102708_create_period_table extends Migration
      */
     public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable(self::PERIOD, [
             'period_id' => $this->primaryKey()->unsigned(),
             'user_id' => $this->string()->notNull(),
             'period_start' => $this->dateTime()->defaultValue(null),
             'period_end' => $this->dateTime()->defaultValue(null),
-        ], 'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        ], $tableOptions);
     }
 
     /**
